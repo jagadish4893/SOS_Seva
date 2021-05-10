ebf70cd74273a06c1acc4e4f90792716fb6204dd
Jagadisa Padhy <jp00628438@techmahindra.com>
2021-05-10 10:08:06 +0530

    new changes

--- sos_seva/seva/settings.py
+++ sos_seva/seva/settings.py
@@ -65,7 +65,7 @@ ROOT_URLCONF = 'seva.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'assets','templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
